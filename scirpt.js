7435f6134e643d0fb56b0224a5b6b8af2b76b973
Owencheung0113 <145647908+Owencheung0113@users.noreply.github.com>
2024-01-14 22:54:09 +0800

    Add files via upload

--- scirpt.js
+++ scirpt.js
@@ -46,3 +46,18 @@ slides[i].style.display = "none";
 slides[slideIndex-1].style.display = "block";
 dots[slideIndex-1].className += " active";
 }
+
+
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
